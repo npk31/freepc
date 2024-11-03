9636f4dbf9676a7d141ec040618afdedf67a59e7
Khang Nguyen <npk3110.dev@gmail.com>
2024-11-03 16:38:08 +0700

    update

--- tetCountDown.js
+++ tetCountDown.js
@@ -6,9 +6,9 @@ function createCountDown_Tet(parent){
     const style = document.createElement("style");
     style.innerHTML = `
         @import url('https://fonts.googleapis.com/css2?family=Pacifico&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
-    
-        *{
-            color: #fff
+
+        .countdown-tet{
+            color: #fff;
         }
     
         :root{
